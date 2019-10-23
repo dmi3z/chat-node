f6cc082d43b94535d57789a82361743569a3b1d5
Dmitry <dmi3z@yandex.ru>
2019-10-24 01:20:26 +0300

    api and sockets

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 var express = require('express');
-// var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 // const MongoClient = require('mongodb').MongoClient;
 // const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
 const WebSocket = require('ws');
-const SocketServer = WebSocket.Server;
 
 const port = process.env.PORT || 3000;
 
-var server = express()
-    .listen(port, () => { console.log('App listen on port: ', port) });
+const app = express();
+const server = app.listen(port, () => { console.log('App listen on port: ', port) });
 
 
-const wss = new SocketServer({ server });
+const wss = new WebSocket.Server({ server });
     
 wss.on('connection', ws => {
     ws.on('message', message => {
@@ -26,9 +25,10 @@ wss.on('connection', ws => {
 
 // ---------- API -------
 
-// app.get('/', (req, res) => {
-//     res.send('Welcome to AngryChat api');
-// });
+
+app.get('/', (req, res) => {
+    res.send('Welcome to AngryChat api');
+});
 
 // ----------------------
 
