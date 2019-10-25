a64ef2f268035c87933ab9343809a2b8c2a5ccdb
Dmitry <dmi3z@yandex.ru>
2019-10-25 19:45:29 +0300

    websockets

--- index.js
+++ index.js
@@ -1,37 +1,21 @@
-var express = require('express');
-const bodyParser = require('body-parser');
-// const MongoClient = require('mongodb').MongoClient;
-// const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
+const express = require('express');
 const WebSocket = require('ws');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
-const server = app.listen(port, () => { console.log('App listen on port: ', port) });
-
+const server = app.listen(port, () => {
+    console.log('Server started on port: ', port);
+});
 
 const wss = new WebSocket.Server({ server });
-    
-wss.on('connection', ws => {
-    ws.on('message', message => {
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
         wss.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(message);
             }
-        })
-    })
-    ws.send('Welcome to Dmi3z websocket');
+        });
+    });
 });
-
-// ---------- API -------
-
-
-app.get('/', (req, res) => {
-    res.send('Welcome to AngryChat api');
-});
-
-// ----------------------
-
-
-
-
